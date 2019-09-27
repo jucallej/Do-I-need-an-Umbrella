e4338f6d6bdf8366eba17bd025375e381e003474
Julian <julianc@rightmove.co.uk>
2019-09-27 16:30:24 +0100

    Renamed typo

--- integration-test/NeedUmbrellaEndpoint.test.js
+++ integration-test/NeedUmbrellaEndpoint.test.js
@@ -1,7 +1,7 @@
 import nock from 'nock';
-import OpenWeatherMapResponse from './mock_responses/5DaysOpenweathermap.json';
-const request = require('supertest');
+import request from 'supertest';
 import App from '../app.js';
+import OpenWeatherMapResponse from './mock_responses/5DaysOpenweathermap.json';
 
 describe('NeedUmbrellaEndpoint', () => {
     const TEST_LATITUDE = '35';
@@ -9,7 +9,7 @@ describe('NeedUmbrellaEndpoint', () => {
     const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
 
     it('returns true', (done) => {
-        const scope = nock('https://api.openweathermap.org')
+        nock('https://api.openweathermap.org')
             .get(`/data/2.5/forecast?lat=${TEST_LATITUDE}&lon=${TEST_LONGITUDE}&APPID=${OPEN_WEATHER_MAP_API_KEY}`)
             .reply(200, OpenWeatherMapResponse);
 
