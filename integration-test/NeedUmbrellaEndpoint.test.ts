d6de25450fbbd2f0684c6f5092e5ddf7cb7a3abf
Julian <julianc@rightmove.co.uk>
2019-09-30 12:31:44 +0100

    Make different cases for the integration test

--- integration-test/NeedUmbrellaEndpoint.test.ts
+++ integration-test/NeedUmbrellaEndpoint.test.ts
@@ -8,7 +8,23 @@ describe('NeedUmbrellaEndpoint', () => {
     const TEST_LONGITUDE = '139';
     const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
 
-    it('returns true', (done) => {
+    // https://openweathermap.org/weather-conditions
+
+    it.each`
+        weatherId     | result
+        ${200}        | ${true}
+        ${300}        | ${true}
+        ${400}        | ${true}
+        ${500}        | ${true}
+        ${600}        | ${true}
+        ${699}        | ${true}
+        ${700}        | ${false}
+        ${800}        | ${false}
+        `('returns true when all weather ids are equals or higher than 700 (in this case some weather id was set to ' +
+            '$weatherId and it should return $result)', ({weatherId, result}, done) => {
+
+        OpenWeatherMapResponse.list[3].weather[0].id = weatherId;
+
         nock('https://api.openweathermap.org')
             .get(`/data/2.5/forecast?lat=${TEST_LATITUDE}&lon=${TEST_LONGITUDE}&APPID=${OPEN_WEATHER_MAP_API_KEY}`)
             .reply(200, OpenWeatherMapResponse);
@@ -16,7 +32,7 @@ describe('NeedUmbrellaEndpoint', () => {
         request(App)
             .get('/api/needUmbrella')
             .expect(200,
-                { shouldUseUmbrella: false },
+                { shouldUseUmbrella: result },
                 done);
     });
 });
