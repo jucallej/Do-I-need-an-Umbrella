2147d20abc03d223f464597eab4e5083b3dcad21
Julian <julianc@rightmove.co.uk>
2019-10-01 11:52:04 +0100

    Add ts type check to the test

--- bin/www.ts
+++ bin/www.ts
@@ -83,8 +83,9 @@ function onError(error: { syscall: string; code: string; }) {
 
 function onListening() {
   const addr = server.address();
+
   const bind = typeof addr === 'string'
     ? 'pipe ' + addr
-    : 'port ' + addr?.port;
+    : 'port ' + addr!.port;
   debug('Listening on ' + bind);
 }
