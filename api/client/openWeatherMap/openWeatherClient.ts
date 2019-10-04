16bb232ba71ada8980ac7a930f450fb0f1ddde52
Julian <julianc@rightmove.co.uk>
2019-10-04 11:44:50 +0100

    Add the weather client

---
+++ api/client/openWeatherMap/openWeatherClient.ts
@@ -0,0 +1,7 @@
+import fetch from 'node-fetch';
+const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
+
+export const getWeatherData = async (lat: number, long: number) => {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&APPID=${OPEN_WEATHER_MAP_API_KEY}`);
+  return await response.json();
+};
