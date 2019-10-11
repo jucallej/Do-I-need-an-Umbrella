0258555f65555b49fe9452ed6f07f2bbba67dd5a
Julian <julianc@rightmove.co.uk>
2019-10-11 16:53:01 +0100

    Add linting to the FE

--- frontend/useShouldUseUmbrella.ts
+++ frontend/useShouldUseUmbrella.ts
@@ -1,19 +1,33 @@
-import { useState } from "react";
-import useAsyncEffect from "use-async-effect";
-import { UmbrellaState } from "./app";
+import { useState } from 'react';
+import useAsyncEffect from 'use-async-effect';
+import { UmbrellaState } from './app';
 
-export const useShouldUseUmbrella = (lat: number, long: number): UmbrellaState => {
-  const [shouldUseUmbrella, setShouldUseUmbrella] = useState<UmbrellaState>(UmbrellaState.LOADING);
+export const useShouldUseUmbrella = (
+    lat: number,
+    long: number
+): UmbrellaState => {
+    const [shouldUseUmbrella, setShouldUseUmbrella] = useState<UmbrellaState>(
+        UmbrellaState.LOADING
+    );
 
-  useAsyncEffect(async (isMounted) => {
-    if(shouldUseUmbrella === UmbrellaState.LOADING) {
-      const response = await fetch(`/api/needUmbrella?lat=${lat}&lon=${long}`);
-      const json = await response.json();
-      if (!isMounted()) return;
+    useAsyncEffect(
+        async isMounted => {
+            if (shouldUseUmbrella === UmbrellaState.LOADING) {
+                const response = await fetch(
+                    `/api/needUmbrella?lat=${lat}&lon=${long}`
+                );
+                const json = await response.json();
+                if (!isMounted()) return;
 
-      setShouldUseUmbrella(json.shouldUseUmbrella ? UmbrellaState.USE_UMBRELLA : UmbrellaState.DO_NOT_USE_UMBRELLA);
-    }
-  }, [shouldUseUmbrella]);
+                setShouldUseUmbrella(
+                    json.shouldUseUmbrella
+                        ? UmbrellaState.USE_UMBRELLA
+                        : UmbrellaState.DO_NOT_USE_UMBRELLA
+                );
+            }
+        },
+        [shouldUseUmbrella]
+    );
 
-  return shouldUseUmbrella;
+    return shouldUseUmbrella;
 };
