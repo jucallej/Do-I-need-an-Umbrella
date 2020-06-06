66381ce3e85bda9debdecbfdeac7c7eb6497ad16
Julian Calleja <jucallej@users.noreply.github.com>
2020-06-06 23:28:00 +0100

    Refactor util test to support read only in the latest node type

--- jest-helpers/setupJest.ts
+++ jest-helpers/setupJest.ts
@@ -2,9 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-import {GlobalWithFetchMock} from "jest-fetch-mock";
-const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
-customGlobal.fetch = require('jest-fetch-mock');
-customGlobal.fetchMock = customGlobal.fetch;
+require('jest-fetch-mock').enableMocks();
 
 process.env.OPEN_WEATHER_MAP_API_KEY = 'OPEN_WEATHER_MAP_API_KEY';
