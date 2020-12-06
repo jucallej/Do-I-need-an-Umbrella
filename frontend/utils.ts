134fda3e7c0d3014b9206a92b19e334e45bf7bac
Julian Calleja <jucallej@users.noreply.github.com>
2020-12-06 12:41:40 +0000

    Add type from the getCurrentPosition as typescript was failing to import Coordinates

--- frontend/utils.ts
+++ frontend/utils.ts
@@ -1,6 +1,12 @@
+type PositionCallbackType = Parameters<
+    typeof window.navigator.geolocation.getCurrentPosition
+>[0];
+type PositionType = Parameters<PositionCallbackType>[0];
+type CoordinateType = PositionType['coords'];
+
 export const getCurrentPosition = (
     options = {}
-): Promise<{ coords: Coordinates }> => {
+): Promise<{ coords: CoordinateType }> => {
     return new Promise((resolve, reject) => {
         if ('geolocation' in window.navigator) {
             window.navigator.geolocation.getCurrentPosition(
